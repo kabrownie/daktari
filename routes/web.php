1b91e331531d50742bfd1ef3c48eb3dcc83fae9b
kabrownie <makonokevin@gmail.com>
2025-04-26 16:52:50 +0300

    define routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+use App\Http\Controllers\ProgramController;
+use App\Http\Controllers\ClientController;
+
+Route::get('/programs', [ProgramController::class, 'index']);
+Route::post('/programs', [ProgramController::class, 'store']);
+
+Route::get('/clients', [ClientController::class, 'index']);
+Route::post('/clients', [ClientController::class, 'store']);
+Route::post('/clients/enroll', [ClientController::class, 'enroll']);
+Route::get('/clients/search', [ClientController::class, 'search']);
+Route::get('/clients/{id}', [ClientController::class, 'show']);
+
+// API Route
+Route::get('/api/clients/{id}', [ClientController::class, 'profile']);
